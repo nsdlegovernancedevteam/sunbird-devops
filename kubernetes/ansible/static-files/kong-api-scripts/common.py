eb404e0fcd2f12efa8d157958388cb74ae68919b
NiranjanV12 <niranjanvaity@gmail.com>
2022-02-17 17:05:25 +0530

    py3

--- kubernetes/ansible/static-files/kong-api-scripts/common.py
+++ kubernetes/ansible/static-files/kong-api-scripts/common.py
@@ -1,4 +1,4 @@
-import urllib2, json, logging
+import  urllib.error,urllib.request, urllib3, json, logging
 from retry import retry
 
 logging.basicConfig()
@@ -29,13 +29,15 @@ def get_api_plugins(kong_admin_api_url, api_name):
 
 def json_request(method, url, data=None):
     request_body = json.dumps(data) if data is not None else None
-    request = urllib2.Request(url, request_body)
+    #request = urllib3.Request(url, request_body)
+    binary_data = request_body.encode('utf8')
+    request = urllib.request.Request(url, binary_data)
     if data:
         request.add_header('Content-Type', 'application/json')
     request.get_method = lambda: method
     response = retrying_urlopen(request)
     return response
 
-@retry(exceptions=urllib2.URLError, tries=5, delay=2, backoff=2)
+@retry(exceptions=urllib.error.URLError, tries=5, delay=2, backoff=2)
 def retrying_urlopen(*args, **kwargs):
-    return urllib2.urlopen(*args, **kwargs)
+    return urllib.request.urlopen(*args, **kwargs)
